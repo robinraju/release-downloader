eac7db2d512ce4a56a1ebaba63b2d0d75e2b6deb
Naveen Prashanth <78990165+gnpaone@users.noreply.github.com>
2024-09-21 09:43:43 +0530

    Update input-helper.ts

--- src/input-helper.ts
+++ src/input-helper.ts
@@ -54,6 +54,10 @@ export function getInputs(): IReleaseDownloadSettings {
     outFilePath: path.resolve(
       githubWorkspacePath,
       core.getInput('out-file-path') || '.'
+    ),
+    extractPath: path.resolve(
+      githubWorkspacePath,
+      core.getInput('extract-path') || core.getInput('out-file-path') || '.'
     )
   }
 }
